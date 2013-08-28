2582ee66af63e245581be2d8fa6a1f3679d3c8c5
Seung Yeon Joo <wntmddussla@hotmail.com>
2013-08-27 22:57:03 -0700

    Update ScreenWatchReceiver.java

--- src/com/example/volt/ScreenWatchReceiver.java
+++ src/com/example/volt/ScreenWatchReceiver.java
@@ -1,78 +1,152 @@
 package com.example.volt;
 
-import java.util.ArrayList;
-
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
-//import com.enspirit.data.toggle.MobileDataToggler;
+public class MenuListAdapter extends BaseAdapter {
 
-/**
- * 
- * BroadcastReceiver that receives screen off and screen on events.
- * 
- * @author Seung Yeon Joo
- *
- */
-public class ScreenWatchReceiver extends BroadcastReceiver {
+        Context context;
+        String[] mTitle;
+        LayoutInflater inflater;
+        DataStore DS;
+        int currentProgress, newProgressValue;
 
-  final public static String ONE_TIME = "onetime";
-	//MobileDataToggler dataToggler;
-	DataStore DS;
-	AlarmManager am;
-	PendingIntent pi1, pi2, pi3;
-	Intent intent1, intent2, intent3;
-	ArrayList<PendingIntent> intentArray;
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		//DS = new DataStore(context);
-		//dataToggler = new MobileDataToggler(context);
-		if (Intent.ACTION_SCREEN_OFF == intent.getAction()) {
-			
-			//dataToggler.toggleMobileData(true);
-			// when screen off is received from intent, 3g is turned on
-			TurnOnOff3GForXmins(context);
-		} else if (Intent.ACTION_SCREEN_ON == intent.getAction()) {
-			am.cancel(pi1);
-			am.cancel(pi2);
-			am.cancel(pi3);
-			//dataToggler.toggleMobileData(true);
-		}
-		
-	}
-	public void TurnOnOff3GForXmins(Context context)
-	{
-		long i, i1;
-		DS = new DataStore(context);
-		//Turn off 3g after 10 mins
-		am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-		intentArray = new ArrayList<PendingIntent>();
-		intent1 = new Intent(context, TurnOnOff3GReceiver.class);
-		intent1.setAction("turn_off_3g");
-		pi1 = PendingIntent.getBroadcast(context, 0, intent1, 0);
-		i = System.currentTimeMillis() + 10 * 60 * 1000;
-		am.set(AlarmManager.RTC_WAKEUP, i, pi1);
-		intentArray.add(pi1);
-		
-		//Turn on 3G back on after 30 mins
-		intent2 = new Intent(context, TurnOnOff3GReceiver.class);
-		intent2.setAction("turn_on_3g");
-		pi2 = PendingIntent.getBroadcast(context, 1, intent2, 0);
-		am.set((int) i, i + (3 * i), pi2);
-		intentArray.add(pi2);
-		i1 = i + (3 * i);
-		
-		//Turn off 3G after 20 seconds
-		intent3 = new Intent(context, TurnOnOff3GReceiver.class);
-		intent3.setAction("turn_off_3g");
-		pi3 = PendingIntent.getBroadcast(context, 2, intent3, 0);
-		am.set((int) i1, i1 + 20000, pi3);
-		intentArray.add(pi3);
-		
-		//End of alarm cycle
-	}
+        public MenuListAdapter(Context context, String[] title) {
+                this.context = context;
+                this.mTitle = title;
+        }
+        @Override
+        public int getCount() {
+                // TODO Auto-generated method stub
+                return mTitle.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+                // TODO Auto-generated method stub
+                return mTitle[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+                // TODO Auto-generated method stub
+                return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+                // TODO Auto-generated method stub
+                final TextView txtTitle;
+                DS = new DataStore(context);
+                SeekBar seekbar;
+                View itemView = null;
+                if(position == 0) {
+                        String x = "Wait " + Integer.toString(DS.getSeekBarSetting1()) + " minutes before turning off data";
+                        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        itemView = inflater.inflate(R.layout.drawer_list_item0, parent, false);
+                        txtTitle = (TextView) itemView.findViewById(R.id.title1);
+                        txtTitle.setText(x);
+                        txtTitle.setTextColor(0xff000000);
+                        seekbar = (SeekBar) itemView.findViewById(R.id.seekbar1);
+                        if(DS.getSeekBarSetting1() == 0)
+                                currentProgress = 5;
+                        else
+                                currentProgress = DS.getSeekBarSetting1();
+
+                        seekbar.setProgress(currentProgress);
+                        seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+                        {
+                                public void onProgressChanged(SeekBar seekbar1, int progress, boolean fromUser) {
+                                        // seekbar has a range between 0 to 100
+                                        String x = "Wait " + Integer.toString(seekbar1.getProgress() + 1) + " minutes before turning off data";
+                                        txtTitle.setText(x);
+                                        txtTitle.setTextColor(0xff000000);
+
+                                }
+                                public void onStartTrackingTouch(SeekBar seekbar1) {
+
+                                }
+                                public void onStopTrackingTouch(SeekBar seekbar1) {
+
+                                        newProgressValue = seekbar1.getProgress();
+                                        currentProgress = newProgressValue;
+                                        DS.setSeekBarSetting1(seekbar1.getProgress() + 1);
+                                }
+                        });
+                } else if(position == 1) {
+                        String x = "Turn on data every " + Integer.toString(DS.getSeekBarSetting2()) + " minutes";
+                        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        itemView = inflater.inflate(R.layout.drawer_list_item1, parent, false);
+                        txtTitle = (TextView) itemView.findViewById(R.id.title2);
+                        txtTitle.setText(x);
+                        txtTitle.setTextColor(0xff000000);
+                        if(DS.getSeekBarSetting2() == 0)
+                                currentProgress = 10;
+                        else
+                                currentProgress = DS.getSeekBarSetting2();
+
+                        seekbar = (SeekBar) itemView.findViewById(R.id.seekbar2);
+                        seekbar.setProgress(currentProgress);
+                        seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+                        {
+                                public void onProgressChanged(SeekBar seekbar1, int progress, boolean fromUser) {
+                                        // seekbar has a range between 0 to 100
+                                        //long time = progress * 60000 ;
+
+                                        //DS.setSeekBarSetting1(progress);
+                                        String x = "Turn on data every " + Integer.toString(seekbar1.getProgress()+ 1) + " minutes";
+                                        txtTitle.setText(x);
+                                        txtTitle.setTextColor(0xff000000);
+                                }
+                                public void onStartTrackingTouch(SeekBar seekbar1) {
+
+                                }
+                                public void onStopTrackingTouch(SeekBar seekbar1) {
+                                        newProgressValue = seekbar1.getProgress();
+                                        currentProgress = newProgressValue;
+                                        DS.setSeekBarSetting2(seekbar1.getProgress() + 1);
+                                }
+                        });
+                } else if(position == 2) {
+                        String x = "Turn on data for " + Integer.toString(DS.getSeekBarSetting3()) + " seconds";
+                        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        itemView = inflater.inflate(R.layout.drawer_list_item2, parent, false);
+                        txtTitle = (TextView) itemView.findViewById(R.id.title3);
+                        txtTitle.setText(x);
+                        txtTitle.setTextColor(0xff000000);
+                        if(DS.getSeekBarSetting3() == 0)
+                                currentProgress = 10;
+                        else
+                                currentProgress = DS.getSeekBarSetting3();
+                        seekbar = (SeekBar) itemView.findViewById(R.id.seekbar3);
+                        seekbar.setProgress(currentProgress);
+                        seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+                        {
+                                public void onProgressChanged(SeekBar seekbar1, int progress, boolean fromUser) {
+                                        // seekbar has a range between 0 to 100
+                                        //long time = progress * 60000 ;
+                                        //DS.setSeekBarSetting1(progress);
+                                        String x = "Turn on data for " + Integer.toString(seekbar1.getProgress() + 3) + " seconds";
+                                        txtTitle.setText(x);
+                                        txtTitle.setTextColor(0xff000000);
+                                }
+                                public void onStartTrackingTouch(SeekBar seekbar1) {
+
+                                }
+                                public void onStopTrackingTouch(SeekBar seekbar1) {
+                                        newProgressValue = seekbar1.getProgress();
+                                        currentProgress = newProgressValue;
+                                        DS.setSeekBarSetting3(seekbar1.getProgress() + 3);
+                                }
+                        });
+                }
+                return itemView;
+        }
+
 }
